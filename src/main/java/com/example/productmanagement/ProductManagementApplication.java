9e4f1f21865e00923408994ba0af479e415ef79d
teja <teja.kalimera@thoughtworks.com>
2024-03-01 12:17:18 +0530

    [Teja] Added product entity and conected to mysql database

--- src/main/java/com/example/productmanagement/ProductManagementApplication.java
+++ src/main/java/com/example/productmanagement/ProductManagementApplication.java
@@ -3,6 +3,7 @@ package com.example.productmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProductManagementApplication {
 
---
+++ src/main/java/com/example/productmanagement/entities/Product.java
@@ -0,0 +1,59 @@
+package com.example.productmanagement.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    private String name;
+    @Size(max = 100)
+    private String description;
+    @Min(1)
+    private int price;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+
+}
