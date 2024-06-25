51a37161d65f03f9d3cca165b38270af5acab09e
teja <tk_kalimera@apple.com>
2024-06-25 23:14:08 +0530

    Add. comment to trigger pipeline

--- product-management/src/main/java/com/example/productmanagement/ProductManagementApplication.java
+++ product-management/src/main/java/com/example/productmanagement/ProductManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductManagementApplication {
-
+//	comment to trigger github pipeline
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagementApplication.class, args);
 	}
