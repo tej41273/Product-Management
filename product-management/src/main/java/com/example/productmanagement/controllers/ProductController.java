56692956de22efdf3d89f3f96f9be091af628d2a
teja <tk_kalimera@apple.com>
2024-07-02 15:08:54 +0530

    Refactor. code

--- product-management/src/main/java/com/example/productmanagement/controllers/ProductController.java
+++ product-management/src/main/java/com/example/productmanagement/controllers/ProductController.java
@@ -6,49 +6,42 @@ import com.example.productmanagement.entities.Product;
 import com.example.productmanagement.repos.ProductRepository;
 
 import jakarta.validation.Valid;
-import org.hibernate.annotations.Parameter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@RequestMapping("/products")
 public class ProductController {
 
-    @Autowired
-    private ProductRepository productRepository;
-    @RequestMapping(value = "/products",method = RequestMethod.GET)
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
     public List<Product> getProducts(){
         return productRepository.findAll();
     }
 
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.GET)
-    @Cacheable("product-cache")
-    @Transactional(readOnly = true)
+    @GetMapping("/{id}")
     public Product getProduct(@PathVariable("id") int id){
         return productRepository.findById(id).get();
     }
 
-    @RequestMapping(value = "/products",method = RequestMethod.POST)
+    @PostMapping
     public Product createProduct(@Valid @RequestBody Product product){
         return productRepository.save(product);
     }
 
-    @RequestMapping(value = "/products",method = RequestMethod.PUT)
+    @PutMapping
     public Product updateProduct(@RequestBody Product product){
         return productRepository.save(product);
     }
 
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.DELETE)
-    @CacheEvict("product-cache")
+    @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable("id") int id){
         productRepository.deleteById(id);
     }
-
-
-
 }
