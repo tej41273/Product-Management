f34f3d262f9a6f683fc223448c9de0793c4b287e
teja <tk_kalimera@apple.com>
2024-07-09 17:30:23 +0530

    Trigger Workflow

--- product-management/src/main/java/com/example/productmanagement/ProductManagementApplication.java
+++ product-management/src/main/java/com/example/productmanagement/ProductManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductManagementApplication {
+//	scscsdcdscdscd
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagementApplication.class, args);
 	}
